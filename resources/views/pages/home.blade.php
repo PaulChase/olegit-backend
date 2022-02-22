b609c6a6a869e7399823678715701fb70c97767d
Ajonye Paul <ajonyechase@gmail.com>
2022-02-22 16:53:11 +0100

    testing DB connection in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,13 @@
 <div>
   the Home Page of the backend
+  <strong>Database Connected: </strong>
+  @php
+    try {
+        \DB::connection()->getPDO();
+        echo \DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        echo 'None';
+    }
+  @endphp
+
 </div>
